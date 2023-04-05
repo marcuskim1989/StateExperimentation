88358e7da7bb1d39eab47dad04b7cdeeb89ef8a8
Marcus Kim <marcuskim1989@gmail.com>
2023-04-05 16:42:15 -0700

    Initial Commit

---
+++ StateExperimentation/StateExperimentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateExperimentationApp.swift
+//  StateExperimentation
+//
+//  Created by Marcus Y. Kim on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct StateExperimentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
